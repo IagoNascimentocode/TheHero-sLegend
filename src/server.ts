6ee8349c4fc779224ecc33711bbeafeb4b495f1d
IagoNascimentocode <iago_ns@hotmail.com>
2021-07-03 23:34:57 -0300

    Router

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json())
+app.use(router)
 
 app.listen(3721, () => console.log("Server Run"))
