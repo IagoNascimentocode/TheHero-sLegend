be3a8356abce0bb0d7b6c02ec6fb1b5f81849d69
IagoNascimentocode <iago_ns@hotmail.com>
2021-07-09 19:48:41 -0300

    Migration Heroes

---
+++ src/database/migrations/1625867654010-CreateHeroes.ts
@@ -0,0 +1,86 @@
+import { Column, MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateHeroes1625867654010 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "heroes",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+
+                    },
+                    {
+                        name: "type",
+                        type: "varchar"
+                    },
+                    {
+                        name: "life",
+                        type: "numeric"
+                    },
+                    {
+                        name: "damage",
+                        type: "numeric"
+                    },
+                    {
+                        name: "armor",
+                        type: "numeric"
+                    },
+                    {
+                        name: "strength",
+                        type: "numeric"
+                    },
+                    {
+                        name: "agility",
+                        type: "numeric"
+                    },
+                    {
+                        name: "intelligence",
+                        type: "numeric"
+                    },
+                    {
+                        name: "movementSpeed",
+                        type: "numeric"
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid",
+                        isNullable: true
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKUser",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("heroes")
+    }
+
+}
