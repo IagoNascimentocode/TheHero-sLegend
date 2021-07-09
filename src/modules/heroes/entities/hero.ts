54bdecec0c8be3e61cf4273f2ebacdf603d715d7
IagoNascimentocode <iago_ns@hotmail.com>
2021-07-09 20:08:05 -0300

    Entity Hero

---
+++ src/modules/heroes/entities/hero.ts
@@ -0,0 +1,58 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid"
+import { User } from "../../accounts/entities/User";
+
+@Entity("heroes")
+class hero {
+
+ @PrimaryColumn()
+ id: string;
+
+ @Column()
+ name: string;
+
+ @Column()
+ type: string;
+
+ @Column()
+ life: number;
+
+ @Column()
+ damage: number;
+
+ @Column()
+ armor: number;
+
+ @Column()
+ strength: number;
+
+ @Column()
+ agility: number;
+
+ @Column()
+ intelligence: number;
+
+ @Column()
+ movementSpeed: number;
+
+ @Column()
+ user_id: string;
+
+ @ManyToOne(() => User)
+ @JoinColumn({ name: "user_id" })
+ user: User;
+
+ @CreateDateColumn()
+ created_at: Date;
+
+ @UpdateDateColumn()
+ updated_at: Date
+
+ constructor() {
+  if (!this.id) {
+   this.id = uuid();
+  }
+ }
+}
+
+export { hero }
