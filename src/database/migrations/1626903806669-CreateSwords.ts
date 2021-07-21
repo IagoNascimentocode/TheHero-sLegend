291add17f4ff671564278bafca850f925dcd4170
IagoNascimentocode <iago_ns@hotmail.com>
2021-07-21 19:40:10 -0300

    Created Swords migration/entity

---
+++ src/database/migrations/1626903806669-CreateSwords.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table, Timestamp } from "typeorm";
+
+export class CreateSwords1626903806669 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "swords",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "damage",
+                        type: "number"
+                    },
+                    {
+                        name: "weight",
+                        type: "number"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("swords")
+    }
+
+}
---
+++ src/modules/items/swords/entities/Sword.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid"
+
+@Entity("swords")
+class Sword {
+
+ @PrimaryColumn()
+ id: string;
+
+ @Column()
+ name: string;
+
+ @Column()
+ damage: number;
+
+ @Column()
+ weight: number;
+
+ @CreateDateColumn()
+ created_at: Date;
+
+ @UpdateDateColumn()
+ updated_at: Date;
+
+ constructor() {
+  if (!this.id) {
+   this.id = uuid();
+  }
+ }
+}
+
+export { Sword }
